4399774beb029dddf3bef904a573fd0a5e65f3e0
Dell_MVMS <companymvms@gmail.com>
2019-02-16 14:56:00 +0100

    style: design improve responsive design

--- js/app.js
+++ js/app.js
@@ -138,8 +138,8 @@ class Player {
 		this.sprite = "images/" + figure;
 		this.x = x;
 		this.y = y;
-    /**very important variable to avoid to open the side-win many times!*/
-    this.loop = true;
+		/**very important variable to avoid to open the side-win many times!*/
+		this.loop = true;
 	}
 	/**The same as constructor however you do not need to delete it.*/
 	restart(x = 303, y = 990, figure = "char-boy.png") {
@@ -149,9 +149,9 @@ class Player {
 	}
 	win() {
 		clearTimeout(oTimer.elapsedTimer);
-      /*it should play it only one time!*/
-      this.loop = false;
-			win.play();
+		/*it should play it only one time!*/
+		this.loop = false;
+		win.play();
 		/*Deactivate the keyboard: because the user have to click with
 		the mouse to avoid going out from the winMenu!*/
 		document.removeEventListener('keyup', gKeys, true);
@@ -160,7 +160,7 @@ class Player {
 	/**If it reaches the water*/
 	update() {
 		if (this.y <= 35 && this.loop) {
-      this.win();
+			this.win();
 		}
 		document.querySelectorAll(".span-moviments").forEach(function(val) {
 			val.textContent = oGame.moviments;
@@ -172,7 +172,7 @@ class Player {
 	}
 	/**Reset the player position*/
 	reset() {
-    this.loop = true;
+		this.loop = true;
 		if (restartMusic.paused) {
 			restartMusic.play();
 		}
@@ -284,13 +284,13 @@ class Game {
 
 		allItems.forEach(function(item, num) {
 			if ((Math.abs(player.x - item.x) == 10) && (Math.abs(player.y - item.y) == 40)) {
-        /**Problems with iphone or smartphone. Sometimes it play and sometimes not.
-        I am trying to see if this code below helps */
-        if (itemMusic.paused) {
-          itemMusic.play();
-        } else {
-          itemMusic.pause();
-        }
+				/**Problems with iphone or smartphone. Sometimes it play and sometimes not.
+				I am trying to see if this code below helps */
+				if (itemMusic.paused) {
+					itemMusic.play();
+				} else {
+					itemMusic.pause();
+				}
 				oGame.moviments = oGame.moviments + 50;
 				allItems.splice(num, 1);
 				oGame.flag = true;
@@ -333,7 +333,7 @@ class Game {
 		/**Show the princess in the castel*/
 		/**Creating the Princess Player*/
 		ctx.drawImage(Resources.get(playerPrincess.sprite), this.arrayXPrincess[oGame.randomNumberX], playerPrincess.y);
-  }
+	}
 
 	throwItems(numbers = 7) {
 		let sprite = new Array("images/GemBlue.png", "images/Heart.png", "images/Key.png",
